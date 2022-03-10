5a4a1e458e532102dc11de360c01765e69a711a4
suba327777 <suba.1357@ezweb.ne.jp>
2022-03-10 21:29:11 +0900

    QiitaApiの認証情報

---
+++ src/common/api/qiita/QiitaApiClient.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
+
+const qiitaAccessToken = process.env.QIITA_ACCESS_TOKEN;
+
+// // リクエストヘッダ+認証情報
+export const QiitaApiClient = axios.create({
+  baseURL: "https://qiita.com/api/v2",
+  responseType: "json",
+  headers: {
+    "Content-Type": "application/json",
+    // 認証
+    Authorization: `Bearer ${qiitaAccessToken}`,
+  },
+});
