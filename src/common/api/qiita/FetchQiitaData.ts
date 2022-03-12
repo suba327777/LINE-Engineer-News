199505956442cdbe587ff45f9e0c5cf5485fc074
suba327777 <suba.1357@ezweb.ne.jp>
2022-03-12 23:24:24 +0900

    検索範囲を詳細に

--- src/common/api/qiita/FetchQiitaData.ts
+++ src/common/api/qiita/FetchQiitaData.ts
@@ -1,23 +1,35 @@
 import { QiitaApiClient } from "./QiitaApiClient";
+import { QiitaItem, QiitaItemResponse } from "./types/QiitaItems";
 
-(async () => {
+export const FetchQiitaData = async () => {
   try {
     const nowDate = new Date();
 
-    const fetchqiitaData = await QiitaApiClient.get("/items", {
+    let setTitle: any = [];
+    let setUrl: any = [];
+
+    const stocks = "stocks:>=10";
+
+    await QiitaApiClient.get<Array<QiitaItemResponse>>("/items", {
       params: {
-        //    1件の記事を取得する
-        per_page: 1,
+        //    5件の記事を取得する
+        per_page: 5,
+        query: stocks,
       },
     })
       .then((res) => {
-        console.log(res);
-        // return res;
+        res.data.map((val, i: number) => {
+          setTitle[i] = val.title;
+          setUrl[i] = val.url;
+        });
+        // console.log(res);
       })
       .catch((err: unknown) => {
         console.log(err);
       });
+    console.log(setTitle);
+    return setUrl;
   } catch (err: unknown) {
     console.log(err);
   }
-})();
+};
