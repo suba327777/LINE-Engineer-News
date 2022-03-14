3408e5c0eca8fc2226a3c7d1475ff3ca0224973f
suba327777 <suba.1357@ezweb.ne.jp>
2022-03-14 19:00:46 +0900

    Qiita記事をlineに出力

--- src/common/template/message/QiitaArticleMessage.ts
+++ src/common/template/message/QiitaArticleMessage.ts
@@ -1,15 +1,38 @@
-import { TextMessage } from "@line/bot-sdk";
+import { TextMessage, Message } from "@line/bot-sdk";
 import { FetchQiitaData } from "../../api/qiita/FetchQiitaData";
+import { QiitaItem } from "../../api/qiita/types/QiitaItems";
 
 export const QiitaArticleMessage = async (): Promise<TextMessage> => {
-  const data: Array<string> = await FetchQiitaData();
-  //   console.log("a");
-  console.log(data);
-  data.map((res, i: number) => {
-    res[i];
+  const today: string = new Date().toLocaleDateString("ja-JP");
+  console.log(today);
+  const qiitaArticle: any = await FetchQiitaData();
+  const data: QiitaItem = qiitaArticle;
+
+  let titleMessage = "";
+  let urlMessage = "";
+
+  data.setTitle.map((res) => {
+    const addTitleMessage = res;
+    if (titleMessage == "") {
+      titleMessage = addTitleMessage;
+    } else {
+      titleMessage += "\n\n" + addTitleMessage;
+    }
   });
+
+  data.setUrl.map((res) => {
+    const addUrlMessage = res;
+    if (urlMessage == "") {
+      urlMessage = addUrlMessage;
+    } else {
+      urlMessage += "\n\n" + addUrlMessage;
+    }
+  });
+
+  const message = titleMessage + "\n\n" + urlMessage;
+
   return {
     type: "text",
-    text: "やあ",
+    text: message,
   };
 };
