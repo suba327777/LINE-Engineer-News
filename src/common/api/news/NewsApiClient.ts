d237de8bb66601b7158015499a65ea3ec90461e8
suba327777 <suba.1357@ezweb.ne.jp>
2022-03-14 23:24:15 +0900

    NewsAPIの認証

---
+++ src/common/api/news/NewsApiClient.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
+
+const newsAccessToken = process.env.NEWS_ACCESS_TOKEN;
+export const NewsApiClient = axios.create({
+  baseURL: "https://newsapi.org/v2",
+  responseType: "json",
+  headers: {
+    "Content-Type": "application/json",
+    // 認証
+    Authorization: `Bearer ${newsAccessToken}`,
+  },
+});
