15f616e540cab9bac9c8fd505362082948de7320
suba327777 <suba.1357@ezweb.ne.jp>
2022-03-15 13:41:25 +0900

    リファクタリング

--- src/common/api/news/FetchNewsData.ts
+++ src/common/api/news/FetchNewsData.ts
@@ -7,21 +7,36 @@ export const FetchNewsData = async (): Promise<any> => {
   try {
     let setTitle = [""];
     let setUrl = [""];
+
+    const today = new Date();
+    const yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 1);
+    const ISO = yesterday.toISOString();
+
     await NewsApiClient.get<NewsItemResponse>("/everything", {
       params: {
         //   検索する記事
         q: "プログラミング",
         // 5件の記事を取得する
         pageSize: 10,
+        // 昨日から
+        from: ISO,
+        // 今日まで
+        to: today,
+
         // 人気の記事の順に並び替える
         sortBy: "popularity",
       },
-    }).then((res) => {
-      res.data.articles.map((val, i: number) => {
-        setTitle[i] = val.title;
-        setUrl[i] = val.url;
+    })
+      .then((res) => {
+        res.data.articles.map((val, i: number) => {
+          setTitle[i] = val.title;
+          setUrl[i] = val.url;
+        });
+      })
+      .catch((err: undefined) => {
+        console.log(err);
       });
-    });
 
     return { setTitle, setUrl };
   } catch (err: unknown) {
