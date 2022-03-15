70c2d7285691014775dcc61c3a8e5be8e7a771c8
suba327777 <suba.1357@ezweb.ne.jp>
2022-03-15 13:42:31 +0900

    FetchMessage

---
+++ src/common/module/FetchMessage.ts
@@ -0,0 +1,34 @@
+import { SetItem } from "./types/SetItemType";
+
+export const FetchMessage = async (data: SetItem): Promise<any> => {
+  try {
+    let titleMessage = "";
+    let urlMessage = "";
+    let numberList = 1;
+    data.setTitle.map((res) => {
+      const addTitleMessage = res;
+      if (titleMessage == "") {
+        titleMessage = `[${numberList}]${addTitleMessage}`;
+        numberList++;
+      } else {
+        titleMessage += `\n\n[${numberList}]${addTitleMessage}`;
+        numberList++;
+      }
+    });
+    numberList = 1;
+    data.setUrl.map((res) => {
+      const addUrlMessage = res;
+      if (urlMessage == "") {
+        urlMessage = `[${numberList}]${addUrlMessage}`;
+        numberList++;
+      } else {
+        urlMessage += `\n\n[${numberList}]${addUrlMessage}`;
+        numberList++;
+      }
+    });
+    const message = titleMessage + "\n\n" + urlMessage;
+    return message;
+  } catch (err: unknown) {
+    console.log(err);
+  }
+};
