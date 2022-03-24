073a40e59c477466708a21315f959f28c1900138
suba327777 <suba.1357@ezweb.ne.jp>
2022-03-24 12:51:55 +0900

    コメント

--- src/common/module/types/SetItemType.ts
+++
@@ -1,4 +0,0 @@
-export type SetItem = {
-  setUrl: string[];
-  setTitle: string[];
-};
--- src/common/template/message/GreetingMessage.ts
+++ src/common/template/message/GreetingMessage.ts
@@ -1,3 +1,4 @@
+/*  packages */
 import { TextMessage } from "@line/bot-sdk";
 
 export const GreetingMessage = (): TextMessage => {
