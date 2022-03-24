285b040b3a079067225fb1ce409f85e87d449ef8
suba327777 <suba.1357@ezweb.ne.jp>
2022-03-24 19:28:33 +0900

    CommentMessage

---
+++ src/common/template/message/CommentMessage.ts
@@ -0,0 +1,11 @@
+/*  packages */
+import { TextMessage } from "@line/bot-sdk";
+
+export const GreetingMessage = (): TextMessage => {
+  const today: string = new Date().toLocaleDateString("ja-JP");
+
+  return {
+    type: "text",
+    text: `${today}の記事はこちらです!!`,
+  };
+};
