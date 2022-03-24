3a8aec07f08a9c068711cc28c0dbd4ac9a95a260
suba327777 <suba.1357@ezweb.ne.jp>
2022-03-24 12:49:56 +0900

    NewsItemの型

--- src/common/api/news/types/NewsItemType.ts
+++ src/common/api/news/types/NewsItemType.ts
@@ -1,3 +1,11 @@
+// 使用する型
+export type NewsItem = {
+  title: string;
+  url: string;
+  urlToImage: string;
+};
+
+// NewsApiのレスポンスの　型
 export type NewsItemResponse = {
   status: string;
   totalResults: number;
@@ -10,7 +18,6 @@ export type NewsItemResponse = {
       url: string;
       urlToImage: string;
       publishedAt: string;
-      content: string;
     },
   ];
 };
