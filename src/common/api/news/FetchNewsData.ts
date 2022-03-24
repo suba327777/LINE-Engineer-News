da6ca339ea37c45071639f80c9676d2b8b5fd60d
suba327777 <suba.1357@ezweb.ne.jp>
2022-03-24 12:47:17 +0900

    リファクタ

--- src/common/api/news/FetchNewsData.ts
+++ src/common/api/news/FetchNewsData.ts
@@ -1,5 +1,7 @@
+/* modules */
 import { NewsApiClient } from "./NewsApiClient";
-import { NewsItemResponse } from "./types/NewsItemType";
+/* types */
+import { NewsItem, NewsItemResponse } from "./types/NewsItemType";
 
 export const FetchNewsData = async (): Promise<any> => {
   try {
@@ -12,18 +14,25 @@ export const FetchNewsData = async (): Promise<any> => {
       params: {
         //   検索する記事
         q: "プログラミング OR エンジニア",
-        // 10件の記事を取得する
-        pageSize: 10,
+
+        // 12件の記事を取得する
+        pageSize: 12,
         // 昨日から
         from: formatYesterday,
         // 今日まで
         to: today,
         // 人気の記事の順に並び替える
-        sortBy: "popularity",
+        sortBy: "publishedAt",
       },
     })
       .then((res) => {
-        return res.data;
+        return res.data.articles.map<NewsItem>((val) => {
+          return {
+            title: val.title,
+            url: val.url,
+            urlToImage: val.urlToImage,
+          };
+        });
       })
       .catch((err: unknown) => {
         console.log(err);
